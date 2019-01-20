d54716f90775ac232965c4216b92b0d0ba82533b
Pawel Surowka <thesurix@gmail.com>
2019-01-20 14:00:10 +0100

    Add google repo and x86 arch

--- Charts/TRChartsJava/jni/Application.mk
+++ Charts/TRChartsJava/jni/Application.mk
@@ -2,6 +2,6 @@ APP_PLATFORM := android-21
 APP_STL := gnustl_static
 NDK_TOOLCHAIN_VERSION := clang
 APP_USE_CCP0X := true
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
 APP_OPTIM := debug
 APP_CFLAG := -g -O0
